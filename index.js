c1974edf0220f801d01f2c929eb793001f961556
Matt Tilley <mtilley255@gmail.com>
2018-05-14 15:33:25 -0500

    Page 14 location 223.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var drawCats = function(howManyTimes)
+var a = 99;
+var b = 123;
+
+var c = a + b;
+
+console.log(c);
+
+/*var a = "APPLE"
+
+console.log(a.length);*/
+
+
+/*var drawCats = function(howManyTimes)
 {
     
     for (var i=0; i<howManyTimes; i++)
@@ -9,4 +21,4 @@ var drawCats = function(howManyTimes)
     
 }
 
-drawCats(10);
+drawCats(10);*/
