f7f2ce6640392595f0fb3af422b48b0a286600c5
Matt Tilley <mtilley255@gmail.com>
2018-05-14 15:29:30 -0500

    Page 8 location 304

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.write("IT WORKS");
+var drawCats = function(howManyTimes)
+{
+    
+    for (var i=0; i<howManyTimes; i++)
+    {
+        console.log(i + "=^.^=");
+        
+    }
+    
+}
+
+drawCats(10);
