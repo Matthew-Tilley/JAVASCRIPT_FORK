93c39dbea39457ea448a03cf0d869193089525fc
Matt Tilley <mtilley255@gmail.com>
2018-05-16 16:28:14 -0500

    Page 75 location 1351.

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-var owedMoney = {};
+var movies = 
+    
+    {
+        "Finding Nemo":{
+            releaseDate: 2003,
+            duration: 100,
+            actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+            format: "DVD"
+        },
+        
+        "Star Wars, Return of the Jedi":{
+            releaseDate: 1983,
+            duration: 134,
+            actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+            format: "DVD"
+        },
+        
+        "Harry Potter's Goblet":{
+            releaseDate: 2005,
+            duration: 157,
+            actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+            format: "Blu-ray"
+        }    
+        
+    }
+
+var findingNemo = movies["Finding Nemo"];
+
+console.log("This movie is " + findingNemo.duration + " minutes, and available on " + findingNemo.format);
+
+var cars = {
+    releaseDate: 2006,
+    duration: 117,
+    actors: ["Owen Wilson", "Bonnie Hunt", "Paul Newman"],
+    format: "Blu-ray"
+}
+
+movies["Cars"] = cars;
+
+
+
+
+
+/*var owedMoney = {};
 
 owedMoney["jimmy"] = 5;
 owedMoney["Anna"] = 7;
@@ -6,7 +49,9 @@ owedMoney.jimmy +=3;
 
 
 console.log(owedMoney.jimmy);
-console.log(owedMoney.Jinen);
+console.log(owedMoney.Jinen);*/
+
+
 
 /*var anna = {
     name: "Anna",
